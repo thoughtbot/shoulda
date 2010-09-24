16984cacbbff26413f83f19ca2f5c7aa1326cd27
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-09-24 19:59:55 +0200

    Add MiniTest integration

--- lib/shoulda.rb
+++ lib/shoulda.rb
@@ -4,6 +4,8 @@ if defined?(RSpec)
   require 'shoulda/integrations/rspec2'
 elsif defined?(Spec)
   require 'shoulda/integrations/rspec'
+elsif defined?(MiniTest::Unit)
+  require 'shoulda/integrations/minitest'
 else
   require 'shoulda/integrations/test_unit'
 end
---
+++ lib/shoulda/integrations/minitest.rb
@@ -0,0 +1,19 @@
+require 'shoulda/context'
+require 'shoulda/proc_extensions'
+require 'shoulda/assertions'
+require 'shoulda/macros'
+require 'shoulda/helpers'
+require 'shoulda/autoload_macros'
+require 'shoulda/rails' if defined? RAILS_ROOT
+
+module MiniTest # :nodoc: all
+  class Unit
+    class TestCase
+      include Shoulda::InstanceMethods
+      extend Shoulda::ClassMethods
+      include Shoulda::Assertions
+      extend Shoulda::Macros
+      include Shoulda::Helpers
+    end
+  end
+end
